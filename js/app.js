cbf9d1401aa9dee245b19b1b619b5b7d1aaf8d7d
ABiviano13 <abiviano0@outlook.com>
2023-02-03 16:57:42 +0100

    FEAT esercixe Dadi

--- js/app.js
+++ js/app.js
@@ -11,24 +11,41 @@ console.log('Utente X vuole cercare le mail da parte di questo mittente: ',  mai
 //4. Controllare se le mail del mittete desiderato ci siano
 //5. Stampare la risposta nel div #esito-mail del file html
 if(mailDaCercare === mailPresenti[0]){
-    // console.log('la tua mail è presente.')
-    esitoMailElement.innerHTML = 'la tua mail è presente'  
+    // console.log('la tua mail è presente.');
+    esitoMailElement.innerHTML = 'la tua mail è presente';  
 } else if(mailDaCercare === mailPresenti[1]){
-    // console.log('la tua mail è presente.')
-    esitoMailElement.innerHTML = 'la tua mail è presente'
+    // console.log('la tua mail è presente.');
+    esitoMailElement.innerHTML = 'la tua mail è presente';
 } else if(mailDaCercare === mailPresenti[2]){
-    // console.log('la tua mail è presente.')
-    esitoMailElement.innerHTML = 'la tua mail è presente'
+    // console.log('la tua mail è presente.');
+    esitoMailElement.innerHTML = 'la tua mail è presente';
 } else if(mailDaCercare === mailPresenti[3]){
-    // console.log('la tua mail è presente.')
-    esitoMailElement.innerHTML = 'la tua mail è presente'
+    // console.log('la tua mail è presente.');
+    esitoMailElement.innerHTML = 'la tua mail è presente';
 } else if(mailDaCercare === mailPresenti[4]){
-    // console.log('la tua mail è presente.')
-    esitoMailElement.innerHTML = 'la tua mail è presente'
+    // console.log('la tua mail è presente.');
+    esitoMailElement.innerHTML = 'la tua mail è presente';
 } else if(mailDaCercare === mailPresenti[5]){
-    // console.log('la tua mail è presente.')
-    esitoMailElement.innerHTML = 'la tua mail è presente'
+    // console.log('la tua mail è presente.');
+    esitoMailElement.innerHTML = 'la tua mail è presente';
 } else {
-    // console.log('La mail ricercata non è presente')
-    esitoMailElement.innerHTML = 'la tua mail non è presente'
-}  
+    // console.log('La mail ricercata non è presente');
+    esitoMailElement.innerHTML = 'la tua mail non è presente';
+};  
+
+// DADI
+//1. Generiamo un numero random per l'utente (da 1 a 6)
+let numeroRandomUtente = Math.floor(Math.random() * 6) + 1;
+console.log('Il numero generato random per utente x è :', numeroRandomUtente);
+//2. Generiamo un numero random per il computer (da 1 a 6)
+let numeroRandomComputer = Math.floor(Math.random() * 6) + 1;
+console.log('Il numero generato random per il computer è :', numeroRandomComputer);
+//3. Creiamo un if - else per stabilire chi ha vinto
+//4. Stampiamo in console il vincitore
+if (numeroRandomUtente < numeroRandomComputer){
+    console.log('Hai perso!!!');
+} else if (numeroRandomUtente > numeroRandomComputer){
+    console.log('Hai Vinto!!!');
+} else {
+    console.log('Pareggio!!!');
+}
