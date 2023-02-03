223ff4fa7853a4f49a74785a7e52b82ac61e0e9d
ABiviano13 <abiviano0@outlook.com>
2023-02-03 15:58:59 +0100

    START MAIL

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//MAIL
+//1. Creare una variabile per andarci a prendere il div #esito-mail dall'html
+//2. Creare un array e registrare delle mail
+//3. Chiedere all'utente che mail vuole cercare
+//4. Stampare all'utente l'esito della sua domanda in div #esito-mail
