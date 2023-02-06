f9fd7bf8df428de3acc5321d0c66dac49951aa95
ABiviano13 <abiviano0@outlook.com>
2023-02-06 10:05:38 +0100

    CORRECT mail

--- js/app.js
+++ js/app.js
@@ -3,39 +3,61 @@
 const esitoMailElement = document.getElementById('esito-mail');
 console.log(esitoMailElement);
 //2. Creare un array e registrare delle mail
-let mailPresenti = ['IntesaSanPaolo', 'BurgerKing', 'Boolean', 'Linkedin', 'Stradivarius', 'Amazon'];
+const mailPresenti = ['IntesaSanPaolo', 'BurgerKing', 'Boolean', 'Linkedin', 'Stradivarius', 'Amazon'];
 console.log(mailPresenti);
 //3. Chiedere all'utente che mail vuole cercare
-let mailDaCercare = prompt('Desideri cercare una mail? Quale sarebbe il mittente?');
+const mailDaCercare = prompt('Desideri cercare una mail? Quale sarebbe il mittente?');
 console.log('Utente X vuole cercare le mail da parte di questo mittente: ',  mailDaCercare);
 //4. Controllare se le mail del mittete desiderato ci siano
 //5. Stampare la risposta nel div #esito-mail del file html
-let esito = '';
+let esito = false;
 
-if(mailDaCercare === mailPresenti[0]){
-    // console.log('la tua mail è presente.');
-    esito = 'la tua mail è presente';  
-} else if(mailDaCercare === mailPresenti[1]){
-    // console.log('la tua mail è presente.');
-    esito = 'la tua mail è presente';
-} else if(mailDaCercare === mailPresenti[2]){
-    // console.log('la tua mail è presente.');
-    esito = 'la tua mail è presente';
-} else if(mailDaCercare === mailPresenti[3]){
-    // console.log('la tua mail è presente.');
-    esito = 'la tua mail è presente';
-} else if(mailDaCercare === mailPresenti[4]){
-    // console.log('la tua mail è presente.');
-    esito = 'la tua mail è presente';
-} else if(mailDaCercare === mailPresenti[5]){
-    // console.log('la tua mail è presente.');
+for( let i = 0; i < mailPresenti.length; i++){
+    const mailCorrente = mailPresenti[i];
+    console.log(mailCorrente);
+
+    // const mailUguali = mailCorrente === mailDaCercare;
+    // console.log(mailUguali)
+
+    if(mailCorrente === mailDaCercare){
+        esito = true;
+    }
+}
+
+console.log(esito);
+
+if(esito){
     esito = 'la tua mail è presente';
 } else {
-    // console.log('La mail ricercata non è presente');
     esito = 'la tua mail non è presente';
-};  
+}
 
 esitoMailElement.innerHTML = esito;
+
+// if(mailDaCercare === mailPresenti[0]){
+//     // console.log('la tua mail è presente.');
+//     esito = 'la tua mail è presente';  
+// } else if(mailDaCercare === mailPresenti[1]){
+//     // console.log('la tua mail è presente.');
+//     esito = 'la tua mail è presente';
+// } else if(mailDaCercare === mailPresenti[2]){
+//     // console.log('la tua mail è presente.');
+//     esito = 'la tua mail è presente';
+// } else if(mailDaCercare === mailPresenti[3]){
+//     // console.log('la tua mail è presente.');
+//     esito = 'la tua mail è presente';
+// } else if(mailDaCercare === mailPresenti[4]){
+//     // console.log('la tua mail è presente.');
+//     esito = 'la tua mail è presente';
+// } else if(mailDaCercare === mailPresenti[5]){
+//     // console.log('la tua mail è presente.');
+//     esito = 'la tua mail è presente';
+// } else {
+//     // console.log('La mail ricercata non è presente');
+//     esito = 'la tua mail non è presente';
+// };  
+
+// esitoMailElement.innerHTML = esito;
 
 // DADI
 //1. Generiamo un numero random per l'utente (da 1 a 6)
